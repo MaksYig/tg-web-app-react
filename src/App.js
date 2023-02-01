1afe1aaccdb9f2c06bb3f2da4cc9a633f4647906
Yigal Igor Maksimov <yigal.maksimov@gmail.com>
2023-02-01 13:30:35 +0300

    toggle btn

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import Btn from './components/button/Button';
 import NavBar from './components/Navbar/Navbar';
 import {useTelegram} from './Hooks/useTelegram';
 import './App.css';
+import { useEffect } from 'react';
 
 
 
 
 function App() {
-  const {user,onToggleButton}=useTelegram();
-  
+  const {tg,onToggleButton}=useTelegram();
+  useEffect(()=>{
+    tg.ready();
+  },[])
+
   return (
     <div className="App">
       <NavBar/>
-      <Btn onClick={onToggleButton}/> 
-      <div>{user}</div>
+      <button onClick={onToggleButton}>Toggle </button>
+
     </div>
   );
 }
